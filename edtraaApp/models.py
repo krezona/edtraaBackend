51aebf0509cd9c6d6016a603921e03eddf9341d5
baelthebard42 <078bct012.anjal@pcampus.edu.np>
2024-04-16 19:31:40 +0545

    added models for instructors, lessons and courses

--- edtraaApp/models.py
+++ edtraaApp/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+class Instructor(models.Model):
+    name=models.CharField(max_length=50)
+
+
+class Course(models.Model):
+
+    categories=[('tech', 'Technology')]
+
+    image=models.ImageField(upload_to='images/')
+    title=models.CharField(max_length=100)
+    subTitle=models.CharField(max_length=200)
+    description=models.TextField()
+    aboutCourse=models.CharField(max_length=1000)
+    instructors=models.ManyToManyField(Instructor)
+    duration=models.CharField(max_length=50)
+    category=models.CharField(max_length=50, choices=categories)
+    topics=models.JSONField()
+
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, related_name='lessons', on_delete=models.CASCADE)
+    number = models.IntegerField()
+    topic = models.CharField(max_length=200)
+    timestamp = models.CharField(max_length=10)  
+    lessonVideoLink = models.URLField(blank=True)  
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.number}: {self.topic}"
+
